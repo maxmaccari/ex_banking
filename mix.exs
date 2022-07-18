5749da5289eb0d329029474afa2acded2a15663f
Maxsuel Maccari <maxmaccari@hotmail.com>
2022-07-17 22:50:34 -0400

    Add code checking CI

--- mix.exs
+++ mix.exs
@@ -8,7 +8,10 @@ defmodule ExBanking.MixProject do
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
