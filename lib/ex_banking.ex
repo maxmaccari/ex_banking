0c730d73c1525de61791d0119a286c8ae7098554
Maxsuel Maccari <maxmaccari@hotmail.com>
2022-07-19 21:05:08 -0400

    Implement ExBanking.create_user/1

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,18 +1,25 @@
 defmodule ExBanking do
   @moduledoc """
-  Documentation for `ExBanking`.
+  An OTP Banking API.
   """
 
+  alias ExBanking.UserSupervisor
+
   @doc """
-  Hello world.
+  Create a new user with the given name.
 
   ## Examples
 
-      iex> ExBanking.hello()
-      :world
+      iex> ExBanking.create_user("Some User")
+      :ok
 
   """
-  def hello do
-    :world
+  @spec create_user(user :: String.t()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
+  def create_user(user) do
+    case UserSupervisor.start_child(user) do
+      {:ok, _pid} -> :ok
+      {:error, :already_started} -> {:error, :user_already_exists}
+      error -> error
+    end
   end
 end
--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -2,7 +2,25 @@ defmodule ExBankingTest do
   use ExUnit.Case
   doctest ExBanking
 
-  test "greets the world" do
-    assert ExBanking.hello() == :world
+  describe "create_user/1" do
+    test "should create a new user if it doesn't exist" do
+      assert :ok = ExBanking.create_user(random_name())
+    end
+
+    test "should not allow creating a user that exists" do
+      used_name = random_name()
+      assert :ok = ExBanking.create_user(used_name)
+      assert {:error, :user_already_exists} = ExBanking.create_user(used_name)
+    end
+
+    test "should allow creating user only with string args" do
+      assert {:error, :wrong_arguments} = ExBanking.create_user(123)
+    end
+  end
+
+  defp random_name do
+    20
+    |> :rand.bytes()
+    |> Base.url_encode64(padding: false)
   end
 end
