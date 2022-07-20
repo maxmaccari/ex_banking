23f267dd9840f91206111dd1caeea39c9bf71134
Maxsuel Maccari <maxmaccari@hotmail.com>
2022-07-19 21:48:17 -0400

    Add main functions to public interface

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -3,7 +3,7 @@ defmodule ExBanking do
   An OTP Banking API.
   """
 
-  alias ExBanking.UserSupervisor
+  alias ExBanking.{User, UserSupervisor}
 
   @doc """
   Create a new user with the given name.
@@ -18,8 +18,65 @@ defmodule ExBanking do
   def create_user(user) do
     case UserSupervisor.start_child(user) do
       {:ok, _pid} -> :ok
-      {:error, :already_started} -> {:error, :user_already_exists}
       error -> error
     end
   end
+
+  @doc """
+  Deposit a value to the given user.
+
+  ## Examples
+
+      iex> ExBanking.create_user("Some Deposit User")
+      :ok
+      iex> ExBanking.deposit("Some Deposit User", 100.0, "USD")
+      {:ok, 100.0}
+
+  """
+  @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  defdelegate deposit(user, amount, currency), to: User
+
+  @doc """
+  Withdraw a value from the given user.
+
+  ## Examples
+
+      iex> ExBanking.create_user("Some Withdraw User")
+      :ok
+      iex> ExBanking.deposit("Some Withdraw User", 100.0, "USD")
+      {:ok, 100.0}
+      iex> ExBanking.withdraw("Some Withdraw User", 50.0, "USD")
+      {:ok, 50.0}
+
+  """
+  @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :user_does_not_exist
+             | :not_enough_money
+             | :too_many_requests_to_user}
+  defdelegate withdraw(user, amount, currency), to: User
+
+  @doc """
+  Withdraw a value from the given user.
+
+  ## Examples
+
+      iex> ExBanking.create_user("Some Balance User")
+      :ok
+      iex> ExBanking.get_balance("Some Balance User", "USD")
+      {:ok, 0.0}
+      iex> ExBanking.deposit("Some Balance User", 100.0, "USD")
+      {:ok, 100.0}
+      iex> ExBanking.get_balance("Some Balance User", "USD")
+      {:ok, 100.0}
+
+  """
+  @spec get_balance(user :: String.t(), currency :: String.t()) ::
+          {:ok, balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  defdelegate get_balance(user, currency), to: User, as: :balance
 end
--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -18,6 +18,47 @@ defmodule ExBankingTest do
     end
   end
 
+  describe "deposit/3, withdraw/3, and get_balance/2" do
+    test "should allow to deposit, withdraw and get_balance from user" do
+      name = random_name()
+      assert :ok = ExBanking.create_user(name)
+
+      assert {:ok, 100.0} = ExBanking.deposit(name, 100, "USD")
+      assert {:ok, 50.0} = ExBanking.withdraw(name, 50, "USD")
+      assert {:ok, 50.0} = ExBanking.get_balance(name, "USD")
+      assert {:error, :not_enough_money} = ExBanking.withdraw(name, 100, "USD")
+      assert {:ok, 50.0} = ExBanking.get_balance(name, "USD")
+    end
+
+    test "should one currency not interfere in another" do
+      name = random_name()
+      assert :ok = ExBanking.create_user(name)
+
+      assert {:ok, 100.0} = ExBanking.deposit(name, 100, "USD")
+      assert {:ok, 50.0} = ExBanking.deposit(name, 50, "GBP")
+      assert {:ok, 50.0} = ExBanking.withdraw(name, 50, "USD")
+      assert {:ok, 0.0} = ExBanking.withdraw(name, 50, "GBP")
+      assert {:ok, 50.0} = ExBanking.get_balance(name, "USD")
+      assert {:ok, 0.0} = ExBanking.get_balance(name, "GBP")
+    end
+
+    test "should return errors if user doesn't exist" do
+      name = random_name()
+
+      assert {:error, :user_does_not_exist} = ExBanking.deposit(name, 100, "USD")
+      assert {:error, :user_does_not_exist} = ExBanking.withdraw(name, 50, "USD")
+      assert {:error, :user_does_not_exist} = ExBanking.get_balance(name, "GBP")
+    end
+
+    test "should return wrong arguments error" do
+      name = random_name()
+
+      assert {:error, :wrong_arguments} = ExBanking.deposit(name, "100", "USD")
+      assert {:error, :wrong_arguments} = ExBanking.withdraw(name, "50", "USD")
+      assert {:error, :wrong_arguments} = ExBanking.get_balance(name, 123)
+    end
+  end
+
   defp random_name do
     20
     |> :rand.bytes()
